21ec7800ef6c5b3bb70e2969b5286bfff646bfe7
ramesh <ramesh@fcodelabs.com>
2020-04-29 19:22:58 +0530

    index

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+import functions = require("firebase-functions");
+import admin = require("firebase-admin");
+import {DBUtil} from "./util/dBUtil";
+import {CreateAgeHandler} from "./services/create_age/createAgeHandler";
+
+if (admin.apps.length === 0) admin.initializeApp();
+
+export const createAge = functions.firestore.document(`${DBUtil.CHILD}/{childId}`)
+  .onCreate(new CreateAgeHandler().onCreate);
