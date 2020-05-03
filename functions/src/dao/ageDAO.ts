65dcb3005b79fc1044468f1d235d846376162f21
ramesh <ramesh@fcodelabs.com>
2020-05-03 08:25:38 +0530

    dao updates

--- functions/src/dao/ageDAO.ts
+++ functions/src/dao/ageDAO.ts
@@ -2,6 +2,7 @@ import * as admin from "firebase-admin";
 import {Age} from "../models/age";
 import {DBUtil} from "../util/dBUtil";
 import {AgeMapper} from "../mapper/ageMapper";
+import {DocumentReference} from '@google-cloud/firestore'
 
 export class AgeDAO {
   firestore = admin.firestore();
@@ -10,6 +11,26 @@ export class AgeDAO {
   async add(age: Age) {
     const collectionRef = this.firestore.collection(DBUtil.AGE);
     const docRef = (age.id === null || age.id === undefined) ? collectionRef.doc() : collectionRef.doc(age.id);
+    age.ref = docRef;
     await docRef.set(this.mapper.toMap(age));
+  }
+
+  async update(age: Age) {
+    if (age.ref === null || age.ref === undefined) {
+      await this.add(age);
+    } else {
+      await age.ref.update(this.mapper.toMap(age));
+    }
+  }
+
+  async delete(age: Age) {
+    if (age.ref !== null && age.ref !== undefined) {
+      await age.ref.delete();
+    }
+  }
+
+  async getFromRef(ref: DocumentReference) : Promise<Age | undefined> {
+    const snap = await ref.get();
+    return this.mapper.fromSnapshot(snap);
   }
 }
